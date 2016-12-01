c354de5e5629526c0592c10d8a3ace04df799463
Manuel Martinez Marcos <isc.ma3.unix@hotmail.com>
2016-12-01 16:20:59 -0600

    Problema 3 validar vida de celdas.

--- ConCeldas/Program.cs
+++ ConCeldas/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConCeldas
@@ -10,6 +11,39 @@ namespace ConCeldas
     {
         static void Main(string[] args)
         {
+            int tamanoMundo = 50;
+
+            Simulacion sim = new Simulacion(tamanoMundo);
+
+            Random rnd = new Random();
+
+            // intercambiaremos estados ya que inicializan en false(muerto)
+            int intercambios = rnd.Next(1, tamanoMundo);
+
+            for (int i = 0; i < intercambios; i++)
+            {
+                sim.AlternarCelda(rnd.Next(1, tamanoMundo), rnd.Next(1, tamanoMundo));
+            }
+            sim.IniciarGeneracion();
+            Thread.Sleep(100);
+            ImprimirResultados(sim);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimirResultados(Simulacion sim)
+        {
+            var line = new String('-', sim.Tamanio);
+            Console.WriteLine(line);
+
+            for (int y = 0; y < sim.Tamanio; y++)
+            {
+                for (int x = 0; x < sim.Tamanio; x++)
+                {
+                    Console.Write(sim[x, y] ? "1" : "0");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ ConCeldas/Simulacion.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConCeldas
+{
+    public class Simulacion
+    {
+        private bool[,] mundo;
+        private bool[,] siguienteGeneracion;
+        private Task Proceso;
+
+        public int Tamanio { get; private set; }
+        public int Generacion { get; private set; }
+
+        public Simulacion(int tamanio)
+        {
+            Tamanio = tamanio;
+            mundo = new bool[tamanio, tamanio];
+            siguienteGeneracion = new bool[tamanio, tamanio];
+        }
+
+        public Action<bool[,]> siguienteGeneracionFin;
+
+        public bool this[int x, int y]
+        {
+            get { return this.mundo[x, y]; }
+            set { this.mundo[x, y] = value; }
+        }
+
+        public bool AlternarCelda(int x, int y)
+        {
+            bool valorActual = mundo[x, y];
+            return mundo[x, y] = !valorActual;
+        }
+
+        private static int VecinoVivo(bool[,] mundo, int tamanio, int x, int y, int movx, int movy)
+        {
+            int result = 0;
+
+            int propMovX = x + movx;
+            int propMovY = y + movy;
+            bool fueralimite = propMovX < 0 || propMovX >= tamanio | propMovY < 0 || propMovY >= tamanio;
+            if (!fueralimite)
+            {
+                result = mundo[x + movx, y + movy] ? 1 : 0;
+            }
+            return result;
+        }
+
+        private Task ProcesarGeneracion()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Parallel.For(0, Tamanio, x =>
+                {
+                    Parallel.For(0, Tamanio, y =>
+                    {
+                        int numeroVecinos = VecinoVivo(mundo, Tamanio, x, y, -1, 0)
+                            + VecinoVivo(mundo, Tamanio, x, y, -1, 1)
+                            + VecinoVivo(mundo, Tamanio, x, y, 0, 1)
+                            + VecinoVivo(mundo, Tamanio, x, y, 1, 1)
+                            + VecinoVivo(mundo, Tamanio, x, y, 1, 0)
+                            + VecinoVivo(mundo, Tamanio, x, y, 1, -1)
+                            + VecinoVivo(mundo, Tamanio, x, y, 0, -1)
+                            + VecinoVivo(mundo, Tamanio, x, y, -1, -1);
+
+                        bool vivira = false;
+                        bool estaVivo = mundo[x, y];
+
+                        if (estaVivo && (numeroVecinos == 2 || numeroVecinos == 3))
+                        {
+                            vivira = true;
+                        }
+                        else if (!estaVivo && numeroVecinos == 3)
+                        {
+                            vivira = true;
+                        }
+                        siguienteGeneracion[x, y] = vivira;
+                    });
+                });
+            });
+        }
+
+
+        public void Actualizar()
+        {
+            if (Proceso != null && Proceso.IsCompleted)
+            {
+                // cuando la generacion se completo
+                var flip = siguienteGeneracion;
+                siguienteGeneracion = mundo;
+                mundo = flip;
+                Generacion++;
+
+                Proceso = ProcesarGeneracion();
+                if (siguienteGeneracionFin != null)
+                {
+                    siguienteGeneracionFin(mundo);
+                }                  
+            }
+        }
+
+        public void IniciarGeneracion()
+        {
+            if (Proceso == null || (Proceso != null && Proceso.IsCompleted))
+            {
+                // Solo se inicia generacion si el proceso actual se completo o si no se ha inicializado
+                Proceso = this.ProcesarGeneracion();
+            }
+        }
+    }
+}
